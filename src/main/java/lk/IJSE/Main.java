07257fa96a00f04de449a69b8718526627eb651d
WDinithi <wijewardanadinithi@gmail.com>
2023-08-03 13:21:30 +0530

    Many_to_Many

--- src/main/java/lk/IJSE/Main.java
+++ src/main/java/lk/IJSE/Main.java
@@ -1,4 +1,55 @@
 package lk.IJSE;
 
+import lk.IJSE.entity.Author;
+import lk.IJSE.entity.Book;
+import lk.IJSE.utill.FactoryConfuguration;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        Author author= new Author();
+        author.setA_id("A001");
+        author.setFirst_name("Dinithi");
+        author.setLast_name("Chethana");
+
+        author.setA_id("A002");
+        author.setFirst_name("Chethana");
+        author.setLast_name("purnima");
+
+
+        Book book = new Book();
+        book.setB_id("B001");
+        book.setB_name("Sister");
+        book.setISBN("1234");
+
+
+        Book book1=new Book();
+        book1.setB_id("B002");
+        book1.setB_name("hallooo");
+        book1.setISBN("1234");
+
+        List<Book> bookList=new ArrayList<>();
+        bookList.add(book);
+        bookList.add(book1);
+
+        List<Author> authors=new ArrayList<>();
+        bookList.add(book);
+        bookList.add(book1);
+
+        List<Author> authors1=new ArrayList<>();
+        bookList.add((Book) authors);
+
+        Session session= FactoryConfuguration.getInstance().getSession();
+        Transaction transaction=session.beginTransaction();
+
+        session.persist(author);
+        session.persist(book);
+
+        transaction.commit();
+        session.close();
+    }
 }
